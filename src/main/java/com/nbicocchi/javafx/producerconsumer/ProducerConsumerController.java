c8822ed419fe52187df48a80e7f15169f5726af5
Nicola Bicocchi <nbicocchi@unimore.it>
2023-04-11 14:01:21 +0200

    Update

--- src/main/java/com/nbicocchi/javafx/producerconsumer/ProducerConsumerController.java
+++ src/main/java/com/nbicocchi/javafx/producerconsumer/ProducerConsumerController.java
@@ -6,24 +6,22 @@ import javafx.scene.chart.ScatterChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.ChoiceBox;
 
-import java.util.ArrayDeque;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ConcurrentLinkedDeque;
 
 public class ProducerConsumerController {
     public static final int MAX_ITEMS = 25000;
     public static final int MIN_DEQUE = 1;
-    public static final int MAX_DEQUE = 256;
+    public static final int MAX_DEQUE = 128;
     @FXML private ScatterChart<Number, Number> chart;
     @FXML private NumberAxis xAxis;
     @FXML private NumberAxis yAxis;
     @FXML private ChoiceBox<String> chArchitecture;
 
     public void initialize() {
-        String[] experiments = {"Synchronized", "Synchronized2", "Synchronized + W/N", "ConcurrentLinkedDeque",
-                "ArrayBlockingQueue"};
+        String[] experiments = {"LinkedList + Synchronized", "LinkedList + Synchronized + W/N",
+                "ConcurrentLinkedDeque"};
         chArchitecture.getItems().addAll(experiments);
         chArchitecture.getSelectionModel().select(0);
         xAxis.setLowerBound(0);
@@ -36,11 +34,9 @@ public class ProducerConsumerController {
     @FXML
     void onStart() throws InterruptedException {
         switch (chArchitecture.getValue()) {
-            case "Synchronized" -> runExperimentsSafe();
-            case "Synchronized2" -> runExperimentsSafe2();
-            case "Synchronized + W/N" -> runExperimentsSafeWaitNotify();
+            case "LinkedList + Synchronized" -> runExperimentsSafe();
+            case "LinkedList + Synchronized + W/N" -> runExperimentsSafeWaitNotify();
             case "ConcurrentLinkedDeque" -> runExperimentsLinkedDeque();
-            case "ArrayBlockingQueue" -> runExperimentsBlockingQueue();
         }
     }
 
@@ -50,20 +46,6 @@ public class ProducerConsumerController {
     }
 
     void runExperimentsSafe() throws InterruptedException {
-        for (int i = MIN_DEQUE; i < MAX_DEQUE; i++) {
-            final int items = i;
-            Queue<Integer> queue = new ArrayDeque<>();
-            Producer producer = new ProducerSafe(queue, items, MAX_ITEMS);
-            Consumer consumer = new ConsumerSafe(queue, MAX_ITEMS);
-            consumer.setOnSucceeded(event -> {
-                XYChart.Series<Number, Number> data = getSeriesByName(chArchitecture.getValue());
-                data.getData().add(new XYChart.Data<>(items, consumer.getValue()));
-            });
-            runExperiment(producer, consumer);
-        }
-    }
-
-    void runExperimentsSafe2() throws InterruptedException {
         for (int i = MIN_DEQUE; i < MAX_DEQUE; i++) {
             final int items = i;
             Queue<Integer> queue = new LinkedList<>();
@@ -95,20 +77,6 @@ public class ProducerConsumerController {
         for (int i = MIN_DEQUE; i < MAX_DEQUE; i++) {
             final int items = i;
             Queue<Integer> queue = new ConcurrentLinkedDeque<>();
-            Producer producer = new ProducerUnsafe(queue, items, MAX_ITEMS);
-            Consumer consumer = new ConsumerUnSafe(queue, MAX_ITEMS);
-            consumer.setOnSucceeded(event -> {
-                XYChart.Series<Number, Number> data = getSeriesByName(chArchitecture.getValue());
-                data.getData().add(new XYChart.Data<>(items, consumer.getValue()));
-            });
-            runExperiment(producer, consumer);
-        }
-    }
-
-    void runExperimentsBlockingQueue() throws InterruptedException {
-        for (int i = MIN_DEQUE; i < MAX_DEQUE; i++) {
-            final int items = i;
-            Queue<Integer> queue = new ArrayBlockingQueue<>(items);
             Producer producer = new ProducerUnsafe(queue, items, MAX_ITEMS);
             Consumer consumer = new ConsumerUnSafe(queue, MAX_ITEMS);
             consumer.setOnSucceeded(event -> {
