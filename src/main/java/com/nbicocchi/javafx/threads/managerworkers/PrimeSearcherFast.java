f375727bac199540c7b8bb941310e13bff484662
Nicola Bicocchi <nbicocchi@unimore.it>
2023-04-12 22:59:29 +0200

    Update

--- src/main/java/com/nbicocchi/javafx/threads/managerworkers/PrimeSearcherFast.java
+++ src/main/java/com/nbicocchi/javafx/threads/managerworkers/PrimeSearcherFast.java
@@ -1,5 +1,14 @@
 package com.nbicocchi.javafx.threads.managerworkers;
 
+/**
+ * If a number n is not a prime, it can be factored into two factors a and b:
+ * <p>
+ * n = a * b
+ * <p>
+ * Now a and b can't be both greater than the square root of n, since then the product a * b would be greater than
+ * sqrt(n) * sqrt(n) = n. So in any factorization of n, at least one of the factors must be less than or equal to the
+ * square root of n, and if we can't find any factors less than or equal to the square root, n must be a prime.
+ */
 public class PrimeSearcherFast implements PrimeSearcher {
     @Override
     public boolean isPrime(int number) {
