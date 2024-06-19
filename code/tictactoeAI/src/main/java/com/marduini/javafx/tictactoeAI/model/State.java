71d791e496202cb4ff2315edc1f9df85b8a4887d
mikiarduini <mikiarduz2003@gmail.com>
2024-06-19 19:05:12 +0200

    adding the new 'tictactoeAI' project

---
+++ code/tictactoeAI/src/main/java/com/marduini/javafx/tictactoeAI/model/State.java
@@ -0,0 +1,51 @@
+package com.marduini.javafx.tictactoeAI.model;
+
+import java.util.Arrays;
+
+public class State {
+    // This is the class used to represent the states of the board, with a string array
+    private int position;
+    private String[] state;
+
+    public State(int position, String[] state) {
+        this.position = position;
+        this.state = state;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public String[] getState() {
+        return state;
+    }
+
+    /**
+     * returns the state of the i-pane of the current state (String[] state)
+     * @param i the i-pane which we want to take the fill of the board
+     * @return
+     */
+    public String getStateIndex(int i){
+        return state[i];
+    }
+
+    public void setState(String[] state) {
+        this.state = state;
+    }
+
+    public void changeState(int i, String player){
+        state[i] = player;
+    }
+
+    @Override
+    public String toString() {
+        return "State{" +
+                "position=" + position +
+                ", state=" + Arrays.toString(state) +
+                '}';
+    }
+}
