b35a6b00843cd8ccd250a0f547ae957009305d1e
Nicola Bicocchi <nbicocchi@unimore.it>
2024-06-24 20:18:39 +0200

    update

--- code/games-twentyone/src/main/java/com/twentyone/App.java
+++ code/games-twentyone/src/main/java/com/twentyone/App.java
@@ -47,8 +47,6 @@ public class App extends Application {
         return fxmlLoader.load();
     }
 
-
-
     public static void main(String[] args) {
         launch();//starts the application
     }
--- code/games-twentyone/src/main/java/module-info.java
+++ code/games-twentyone/src/main/java/module-info.java
@@ -1,8 +1,5 @@
-module com.twentyone {
+open module com.twentyone {
     requires javafx.controls;
     requires javafx.fxml;
     requires transitive javafx.graphics;
-
-    opens com.twentyone to javafx.fxml;
-    exports com.twentyone;
 }
