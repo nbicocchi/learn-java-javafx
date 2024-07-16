c3b3286c03016680dc0ece4e1a87ad1beaadce58
Nicola Bicocchi <nbicocchi@unimore.it>
2024-07-16 11:24:17 +0200

    update

--- code/fractals-trees/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.treefractals {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.javafx.treefractals to javafx.fxml;
-    exports com.javafx.treefractals;
-}
