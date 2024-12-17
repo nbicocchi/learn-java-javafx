c400107a410c33f29644a39487470ba5490a98a6
Nicola Bicocchi <nbicocchi@unimore.it>
2024-12-17 17:41:06 +0100

    update

--- code/desktop-address-book/src/main/java/module-info.java
+++ code/desktop-address-book/src/main/java/module-info.java
@@ -3,6 +3,7 @@ open module com.nbicocchi.javafx.addressbook {
     requires java.sql;
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.fasterxml.jackson.annotation;
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.datatype.jsr310;
