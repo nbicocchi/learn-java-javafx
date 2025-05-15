98073e19c68e18a6503d809409b3932bcc69f2ca
Nicola Bicocchi <nbicocchi@unimore.it>
2025-05-15 23:34:37 +0200

    update

--- code/math-data-visualization/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-open module com.nbicocchi.javafx.datavis {
-    requires java.desktop;
-    requires java.sql;
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.swing;
-}
